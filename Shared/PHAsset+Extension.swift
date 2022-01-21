1e853dd21d8180d13cd4d6a60e0ff1575e83400c
bannzai <kingkong999yhirose@gmail.com>
2022-01-22 08:05:14 +0900

    :memo: with sorted

--- Shared/PHAsset+Extension.swift
+++ Shared/PHAsset+Extension.swift
@@ -8,7 +8,15 @@ extension PHFetchResult where ObjectType == PHAsset {
         for i in (0..<count) {
             assets.append(object(at: i))
         }
-        return assets
+        return assets.sorted { lhs, rhs in
+            if let l = lhs.creationDate?.timeIntervalSinceReferenceDate,
+                let r = rhs.creationDate?.timeIntervalSinceReferenceDate {
+                return l < r
+            } else {
+                assertionFailure()
+                return false
+            }
+        }
     }
 }
 
