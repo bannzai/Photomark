d4b6d1af2310b934d33559cae2769e29d5a779fb
bannzai <kingkong999yhirose@gmail.com>
2022-01-21 23:40:07 +0900

    :memo: fix factory

--- Shared/CoreDataModelFactory.swift
+++ Shared/CoreDataModelFactory.swift
@@ -1,30 +1,21 @@
 import Foundation
 import CoreData
 
-protocol CoreDataFactory: AnyObject {
-    static func create(context: NSManagedObjectContext) -> Self
-}
-
-extension NSManagedObject: CoreDataFactory {
-    @objc class func create(context: NSManagedObjectContext) -> Self {
-        fatalError("Subclass implementation")
-    }
-}
-
 extension Photo {
-    override class func create(context: NSManagedObjectContext) -> Self {
+    static func create(context: NSManagedObjectContext, imageData: Data) -> Photo {
         let photo = Photo(context: context)
         photo.id = .init()
         photo.createdDate = .init()
-        return photo as! Self
+        photo.imageData = imageData
+        return photo
     }
 }
 
 extension Tag {
-    override class func create(context: NSManagedObjectContext) -> Self {
+    static func create(context: NSManagedObjectContext) -> Tag {
         let tag = Tag(context: context)
         tag.id = .init()
         tag.createdDate = .init()
-        return tag as! Self
+        return tag
     }
 }
