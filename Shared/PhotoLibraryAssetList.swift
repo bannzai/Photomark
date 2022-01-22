a260dac0a0b79ab33b7c5c734ab0f05f87132ba0
bannzai <kingkong999yhirose@gmail.com>
2022-01-23 07:48:08 +0900

    Remove unnecessary GeometryReader

--- Shared/PhotoLibraryAssetList.swift
+++ Shared/PhotoLibraryAssetList.swift
@@ -10,26 +10,24 @@ struct PhotoLibraryAssetList: View {
     .init(.flexible(), spacing: 1),
     .init(.flexible(), spacing: 1),
   ]
-
+  
   var body: some View {
-    GeometryReader { geometry in
-      GeometryReader { viewGeometry in
-        ScrollView(.vertical) {
-          LazyVGrid(columns: gridItems, spacing: 1) {
-            ForEach(assets) { asset in
-              if let image = asset.image {
-                GridImage(image: image)
-              }
+    GeometryReader { viewGeometry in
+      ScrollView(.vertical) {
+        LazyVGrid(columns: gridItems, spacing: 1) {
+          ForEach(assets) { asset in
+            if let image = asset.image {
+              GridImage(image: image)
             }
           }
         }
-        .task {
-          let phAssets = Array(photoLibrary.fetchAssets().assets()[0..<40])
-          for phAsset in phAssets {
-            Task { @MainActor in
-              for await response in photoLibrary.imageStream(for: phAsset, maxImageLength: viewGeometry.size.width / 3) {
-                assets.append(response)
-              }
+      }
+      .task {
+        let phAssets = Array(photoLibrary.fetchAssets().assets()[0..<40])
+        for phAsset in phAssets {
+          Task { @MainActor in
+            for await response in photoLibrary.imageStream(for: phAsset, maxImageLength: viewGeometry.size.width / 3) {
+              assets.append(response)
             }
           }
         }
