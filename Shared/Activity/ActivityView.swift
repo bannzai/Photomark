65e436df4664bad6074ab3d936585a9174d4ff9b
bannzai <kingkong999yhirose@gmail.com>
2022-01-23 21:51:02 +0900

    :sparkles: add activity view

---
+++ Shared/Activity/ActivityView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+import UIKit
+
+struct ActivityView: UIViewControllerRepresentable {
+
+  let images: [UIImage]
+  let onCancel: () -> Void
+  let onComplete: (UIActivity.ActivityType?) -> Void
+
+  public func makeUIViewController(context: Context) -> UIActivityViewController {
+    let activityController = UIActivityViewController(activityItems: images, applicationActivities: nil)
+    activityController.completionWithItemsHandler = {
+      (activityType, completed, returnedItems, error) in
+      if !completed {
+        onCancel()
+      } else {
+        onComplete(activityType)
+      }
+    }
+    return activityController
+  }
+
+  public func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
+    // Noop
+  }
+}
+
