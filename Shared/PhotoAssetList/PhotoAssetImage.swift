1e48c8286775e6501ba8bcffd4aa145d9e91206f
bannzai <kingkong999yhirose@gmail.com>
2022-01-26 22:02:04 +0900

    Revert "Remove download button"
    This reverts commit 612f1f7c33f510fa7809d065b327462462c842b9.

--- Shared/PhotoAssetList/PhotoAssetImage.swift
+++ Shared/PhotoAssetList/PhotoAssetImage.swift
@@ -11,18 +11,23 @@ struct PhotoAssetImage: View {
 
   @State var editingPhoto: Photo? = nil
   @State var error: Error?
-  
+
   var body: some View {
-    AsyncAssetImage(asset: asset, maxImageLength: maxImageLength) { image in
-      image
-        .resizable()
-        .scaledToFill()
-        .frame(width: maxImageLength, height: maxImageLength)
-        .clipped()
-    } placeholder: {
-      Image(systemName: "photo")
+    ZStack(alignment: .bottomTrailing) {
+      AsyncAssetImage(asset: asset, maxImageLength: maxImageLength) { image in
+          image
+            .resizable()
+            .scaledToFill()
+            .frame(width: maxImageLength, height: maxImageLength)
+            .clipped()
+      } placeholder: {
+          Image(systemName: "photo")
+      }
+      .frame(width: maxImageLength, height: maxImageLength)
+
+      AssetDownloadButton(asset: asset)
+        .frame(width: 32, height: 32)
     }
-    .frame(width: maxImageLength, height: maxImageLength)
     .onTapGesture {
       if let photo = photo {
         editingPhoto = photo
