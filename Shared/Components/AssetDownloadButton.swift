9fd53d3f0e7933e1aef955e0e7dc528f188f1825
bannzai <kingkong999yhirose@gmail.com>
2022-01-27 08:21:09 +0900

    Add delay

--- Shared/Components/AssetDownloadButton.swift
+++ Shared/Components/AssetDownloadButton.swift
@@ -17,6 +17,9 @@ struct AssetDownloadButton: View {
           Task { @MainActor in
             if let image = await photoLibrary.highQualityImage(for: asset) {
               UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+
+              // Delay for user can recognize ProgressView.
+              await Task.sleep(2 * (NSEC_PER_SEC / 10))
               isDownloading = false
             } else {
               error = AlertError("画像を保存できませんでした", "再度お試しください")
