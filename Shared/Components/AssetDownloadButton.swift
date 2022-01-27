525902082d3e2c661795ab9bf99e2b29e1c0c368
bannzai <kingkong999yhirose@gmail.com>
2022-01-27 15:29:15 +0900

    Revert "Remove unnecessary Group"
    This reverts commit a9f26ef3444fb4c03f5918203a7c8468ecc3cc5b.

--- Shared/Components/AssetDownloadButton.swift
+++ Shared/Components/AssetDownloadButton.swift
@@ -8,27 +8,29 @@ struct AssetDownloadButton: View {
   @State var error: Error?
 
   var body: some View {
-    if isDownloading {
-      ProgressView()
-    } else {
-      Button(action: {
-        isDownloading = true
+    Group {
+      if isDownloading {
+        ProgressView()
+      } else {
+        Button(action: {
+          isDownloading = true
+          
+          Task { @MainActor in
+            if let image = await photoLibrary.highQualityImage(for: asset) {
+              UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
 
-        Task { @MainActor in
-          if let image = await photoLibrary.highQualityImage(for: asset) {
-            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-
-            // Delay for user can recognize ProgressView.
-            await Task.sleep(2 * (NSEC_PER_SEC / 10))
-            isDownloading = false
-          } else {
-            error = AlertError("画像を保存できませんでした", "再度お試しください")
+              // Delay for user can recognize ProgressView.
+              await Task.sleep(2 * (NSEC_PER_SEC / 10))
+              isDownloading = false
+            } else {
+              error = AlertError("画像を保存できませんでした", "再度お試しください")
+            }
           }
+        }) {
+          Image(systemName: "arrow.down.circle")
         }
-      }) {
-        Image(systemName: "arrow.down.circle")
       }
     }
-  }
     .handle(error: $error)
+  }
 }
