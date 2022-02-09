4242a844f70b241376ddf9d34798a0436fa6c6bf
bannzai <kingkong999yhirose@gmail.com>
2022-02-09 23:00:37 +0900

    Prepare cgImage property

--- Shared/Bridge/UIImage+.swift
+++ Shared/Bridge/UIImage+.swift
@@ -7,5 +7,16 @@ typealias UIImage = UIKit.UIImage
 #if os(macOS)
 import AppKit
 typealias UIImage = AppKit.NSImage
+
+
+extension NSImage {
+  var toCGImage: CGImage {
+    var imageRect = NSRect(origin: .zero, size: size)
+    guard let image = cgImage(forProposedRect: &imageRect, context: nil, hints: nil) else {
+      abort()
+    }
+    return image
+  }
+}
 #endif
 
