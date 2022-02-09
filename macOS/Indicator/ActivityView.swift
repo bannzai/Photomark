24f754be004fe5a91fd2b189992aa7f6fccb71dc
bannzai <kingkong999yhirose@gmail.com>
2022-02-10 08:12:28 +0900

    Add ActivityView

--- macOS/Indicator/ActivityView.swift
+++ macOS/Indicator/ActivityView.swift
@@ -1,15 +1,12 @@
 import SwiftUI
 import AppKit
 
-struct ActivityView: NSViewRepresentable {
+// TODO: Remove 
+struct ActivityView: View {
   let images: [UIImage]
-  var onCancel: (() -> Void)?
-  var onComplete: ((UIActivity.ActivityType?) -> Void)?
 
-  func makeNSView(context: Context) -> some NSView {
-    let view = NSProgressIndicator(frame: .init(origin: .zero, size: .init(width: 40, height: 40)))
-    view.style = .spinning
-    return view
+  var body: some View {
+    EmptyView()
   }
 }
 
