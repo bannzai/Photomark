302557f66af24c8971f51b59ef405391097a2392
bannzai <kingkong999yhirose@gmail.com>
2022-02-11 09:16:30 +0900

    :memo: add navigation link

--- macOS/PhotoAssetList/PhotoAssetImage.swift
+++ macOS/PhotoAssetList/PhotoAssetImage.swift
@@ -12,38 +12,48 @@ struct PhotoAssetImage: View {
   @State var editingPhoto: Photo? = nil
   @State var error: Error?
 
+  var transitionedToDetail: Binding<Bool> {
+    .init {
+      editingPhoto != nil
+    } set: { _ in
+      editingPhoto = nil
+    }
+  }
+
+
   var body: some View {
-    ZStack(alignment: .bottomTrailing) {
-      AsyncAssetImage(asset: asset, maxImageLength: maxImageLength) { image in
+    NavigationLink(isActive: transitionedToDetail) {
+      if let photo = editingPhoto {
+        PhotoDetailPage(asset: asset, photo: photo, tags: tags)
+      }
+    } label: {
+      ZStack(alignment: .bottomTrailing) {
+        AsyncAssetImage(asset: asset, maxImageLength: maxImageLength) { image in
           image
             .resizable()
             .scaledToFill()
             .frame(width: maxImageLength, height: maxImageLength)
             .clipped()
-      } placeholder: {
+        } placeholder: {
           Image(systemName: "photo")
-      }
-      .frame(width: maxImageLength, height: maxImageLength)
+        }
+        .frame(width: maxImageLength, height: maxImageLength)
 
-      AssetCopyButton(asset: asset)
-        .frame(width: 32, height: 32)
-    }
-    .onTapGesture {
-      if let photo = photo {
-        editingPhoto = photo
-      } else {
-        do {
-          editingPhoto = try Photo.createAndSave(context: viewContext, asset: asset)
-        } catch {
-          self.error = error
+        AssetCopyButton(asset: asset)
+          .frame(width: 32, height: 32)
+      }
+      .onTapGesture {
+        if let photo = photo {
+          editingPhoto = photo
+        } else {
+          do {
+            editingPhoto = try Photo.createAndSave(context: viewContext, asset: asset)
+          } catch {
+            self.error = error
+          }
         }
       }
+      .handle(error: $error)
     }
-    .sheet(item: $editingPhoto) { photo in
-      NavigationView {
-        PhotoDetailPage(asset: asset, photo: photo, tags: tags)
-      }
-    }
-    .handle(error: $error)
   }
 }
