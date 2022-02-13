5a8cad52d32f31733689a98ceeb242b3f772f329
bannzai <kingkong999yhirose@gmail.com>
2022-02-13 21:31:44 +0900

    Fix target

---
+++ Shared/PhotoLibrary/PhotoLibraryAuthorization.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+enum PhotoLibraryRequestAuthorizationActionType {
+  case openSetting
+  case noPermission
+}
+
+extension View {
+  func photoLibraryRequestAuthorization() {
+
+  }
+}
---
+++ macOS/PhotoAssetDateList/PhotoAssetDateList.swift
@@ -0,0 +1,71 @@
+import SwiftUI
+
+struct PhotoAssetDateList: View {
+  @Environment(\.managedObjectContext) private var viewContext
+
+  let assets: [Asset]
+  let photos: [Photo]
+  let tags: [Tag]
+  let sections: [AssetSection]
+  init(assets: [Asset], photos: [Photo], tags: [Tag]) {
+    self.assets = assets
+    self.photos = photos
+    self.tags = tags
+    self.sections = createSections(assets: assets, photos: photos, tags: tags)
+  }
+
+  private let gridItems: [GridItem] = [
+    .init(.flexible(), spacing: 1),
+    .init(.flexible(), spacing: 1),
+    .init(.flexible(), spacing: 1),
+  ]
+
+  let sectionHeaderFomatter: DateIntervalFormatter = {
+    let formatter = DateIntervalFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .none
+    return formatter
+  }()
+
+  var body: some View {
+    ZStack {
+      List {
+        ForEach(0..<sections.count) { i in
+          // FIXME: cause out of index when filtering with photo tags
+          if i <= sections.count - 1 {
+            let section = sections[i]
+
+            LazyVGrid(columns: gridItems, spacing: 1) {
+              Section(header: sectionHeader(section)) {
+                ForEach(section.assets) { asset in
+                  let photo = photos.first(where: { $0.phAssetIdentifier == asset.id })
+
+                  GridAssetImageGeometryReader { gridItemGeometry in
+                    PhotoAssetImage(
+                      asset: asset,
+                      photo: photo,
+                      tags: tags,
+                      maxImageLength: gridItemGeometry.size.width
+                    )
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      .listStyle(.plain)
+    }
+  }
+
+  private func sectionHeader(_ section: AssetSection) -> some View {
+    HStack {
+      Text(section.interval, formatter: sectionHeaderFomatter)
+        .font(.system(size: 16))
+        .bold()
+      Spacer()
+    }
+    .padding(.top, 12)
+    .padding(.bottom, 8)
+  }
+}
