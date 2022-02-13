67ecdba2b995d653b4596f58454760200e7d6bf3
bannzai <kingkong999yhirose@gmail.com>
2022-02-13 22:14:35 +0900

    Revert "Demo code"
    This reverts commit 547db4fdb787b01d8f3c13a856117110f6149bd0.

--- Shared/PhotomarkApp.swift
+++ Shared/PhotomarkApp.swift
@@ -1,47 +1,10 @@
 import SwiftUI
-import Combine
-
-final class Object: ObservableObject {
-  @Published var count = 0
-}
 
 @main
 struct PhotomarkApp: App {
-
   var body: some Scene {
     WindowGroup {
-      ContentView()
-        .environmentObject(Object())
-    }
-  }
-}
-
-struct ContentView: View {
-  @EnvironmentObject var object: Object
-  var body: some View {
-    let _ = Self._printChanges()
-    VStack {
-      Text("\(object.count)")
-      ContentView2()
-    }
-  }
-}
-
-struct ContentView2: View {
-  var body: some View {
-    let _ = Self._printChanges()
-    ContentView3()
-  }
-}
-
-struct ContentView3: View {
-  @EnvironmentObject var object: Object
-  var body: some View {
-    let _ = Self._printChanges()
-    Button {
-      object.count += 1
-    } label: {
-      Text("Test")
+        EntryView()
     }
   }
 }
