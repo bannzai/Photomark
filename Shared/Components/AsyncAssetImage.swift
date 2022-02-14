70cc423f9f599732758a489140f75d6abefdfa3b
bannzai <kingkong999yhirose@gmail.com>
2022-02-14 22:06:58 +0900

    :memo: without async/await

--- Shared/Components/AsyncAssetImage.swift
+++ Shared/Components/AsyncAssetImage.swift
@@ -48,13 +48,13 @@ struct AsyncAssetImage<Content: View>: View {
   public var body: some View {
     content(phase)
       .animation(Transaction().animation, value: phase.image)
-      .task(id: asset.id) {
-        await load()
+      .onAppear {
+        load()
       }
   }
 
-  private func load() async {
-    for await image in photoLibrary.imageStream(for: asset, maxImageLength: maxImageLength) {
+  private func load() {
+    photoLibrary.fetchImage(for: asset, maxImageLength: maxImageLength) { image in
       if let image = image {
         phase = .success(Image(uiImage: image))
       }
