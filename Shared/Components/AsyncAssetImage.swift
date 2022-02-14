fce135fb1d86090c2f5debbd76a3b23b6948b557
bannzai <kingkong999yhirose@gmail.com>
2022-02-14 22:07:06 +0900

    Revert ":memo: without async/await"
    This reverts commit 70cc423f9f599732758a489140f75d6abefdfa3b.

--- Shared/Components/AsyncAssetImage.swift
+++ Shared/Components/AsyncAssetImage.swift
@@ -48,13 +48,13 @@ struct AsyncAssetImage<Content: View>: View {
   public var body: some View {
     content(phase)
       .animation(Transaction().animation, value: phase.image)
-      .onAppear {
-        load()
+      .task(id: asset.id) {
+        await load()
       }
   }
 
-  private func load() {
-    photoLibrary.fetchImage(for: asset, maxImageLength: maxImageLength) { image in
+  private func load() async {
+    for await image in photoLibrary.imageStream(for: asset, maxImageLength: maxImageLength) {
       if let image = image {
         phase = .success(Image(uiImage: image))
       }
