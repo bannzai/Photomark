67ff3bcc96d12e27c5e74f166e389858acd4e59a
bannzai <kingkong999yhirose@gmail.com>
2022-02-16 17:03:24 +0900

    Add tag list view

--- macOS/TagList/TagList.swift
+++ macOS/TagList/TagList.swift
@@ -7,14 +7,77 @@ struct TagList: View {
     animation: .default)
   var tags: FetchedResults<Tag>
 
-  var body: some View {
-    List {
-      Text("すべての写真")
+  @State var selectedElements: Set<ListElement> = [.all]
 
-      ForEach(tags) { tag in
-        NavigationLink(tag.Name!, destination: <#T##() -> _#>)
+  enum ListElement: Identifiable, Hashable {
+    case all
+    case tag(Tag)
+
+    static let allUUID = UUID()
+
+    var name: String {
+      switch self {
+      case .all:
+          return "すべての写真"
+      case let .tag(tag):
+        return tag.name!
       }
     }
+
+    var id: UUID {
+      switch self {
+      case .all:
+        return ListElement.allUUID
+      case let .tag(tag):
+        return tag.id!
+      }
+    }
+  }
+
+  var body: some View {
+    ZStack {
+      NavigationLink(
+        isActive: .constant(true), destination: {
+          PhotoAssetListPage(selectedTags: selectedElements.compactMap(mappedTag))
+        },
+        label: {
+          EmptyView()
+        }
+      )
+
+
+      List(allElements, id: \.self, selection: $selectedElements) { tag in
+        Button {
+          if tag == .all {
+            selectedElements = [.all]
+          } else {
+            if let allIndex = selectedElements.firstIndex(of: .all) {
+              selectedElements.remove(at: allIndex)
+            }
+
+            if let index = selectedElements.firstIndex(of: tag) {
+              selectedElements.remove(at: index)
+            } else {
+              selectedElements.insert(tag)
+            }
+          }
+        } label: {
+          Text(tag.name)
+        }
+      }
+      .buttonStyle(.plain)
+    }
+  }
+
+  private var allElements: [ListElement] {
+    [.all] + tags.map { .tag($0) }
+  }
+
+  private func mappedTag(_ listElement: ListElement) -> Tag? {
+    if case let .tag(tag) = listElement {
+      return tag
+    }
+    return nil
   }
 }
 
