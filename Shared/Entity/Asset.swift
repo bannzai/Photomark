5a9723d9709b5bc5cc27eba940b4fbb6f74e5afa
bannzai <kingkong999yhirose@gmail.com>
2022-02-19 05:57:50 +0900

    Revert "Calculated time"
    This reverts commit 2ed3296caa9e4a4aa20d2eedb3f46d27d81257b4.

--- Shared/Entity/Asset.swift
+++ Shared/Entity/Asset.swift
@@ -5,10 +5,7 @@ struct Asset: CustomStringConvertible, Identifiable, Hashable {
   var id: String { asset.localIdentifier }
   var cloudIdentifier: String? {
     get {
-      print("[DEBUG] begin: ", Date().timeIntervalSince1970)
-      let id = try? Photos.PHPhotoLibrary.shared().cloudIdentifierMappings(forLocalIdentifiers: [asset.localIdentifier])[asset.localIdentifier]?.get().stringValue
-      print("[DEBUG] end: ", Date().timeIntervalSince1970)
-      return id
+      try? Photos.PHPhotoLibrary.shared().cloudIdentifierMappings(forLocalIdentifiers: [asset.localIdentifier])[asset.localIdentifier]?.get().stringValue
     }
   }
 
