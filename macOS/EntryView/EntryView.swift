dd853e57f5dc5bdc0cd6b5754437fc6b73b571a8
bannzai <kingkong999yhirose@gmail.com>
2022-02-22 22:13:44 +0900

    WIP showsDetailPage

--- macOS/EntryView/EntryView.swift
+++ macOS/EntryView/EntryView.swift
@@ -3,13 +3,18 @@ import SwiftUI
 struct EntryView: View {
   let persistenceController = PersistenceController.shared
 
+  @State var showsDetailPage = false
+
   var body: some View {
-    NavigationView {
       HSplitView {
+        // Left sidebar
         TagList()
           .environment(\.managedObjectContext, persistenceController.container.viewContext)
-      }
+
+        // Right Sidebar
+        if showsDetailPage {
+          
+        }
     }
   }
 }
-
