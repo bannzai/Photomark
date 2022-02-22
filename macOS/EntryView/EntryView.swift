5486691aded6d44aa6d1f0d9038dee3f5dca56f0
bannzai <kingkong999yhirose@gmail.com>
2022-02-22 22:30:09 +0900

    WIP

--- macOS/EntryView/EntryView.swift
+++ macOS/EntryView/EntryView.swift
@@ -3,17 +3,25 @@ import SwiftUI
 struct EntryView: View {
   let persistenceController = PersistenceController.shared
 
-  @State var showsDetailPage = false
+  var body: some View {
+    RootView()
+      .environment(\.managedObjectContext, persistenceController.container.viewContext)
+  }
+}
+
+struct RootView: View {
+  @Environment(\.managedObjectContext) private var viewContext
+
+  @State var detailPageArgument: Asset?
 
   var body: some View {
       HSplitView {
         // Left sidebar
         TagList()
-          .environment(\.managedObjectContext, persistenceController.container.viewContext)
 
         // Right Sidebar
-        if showsDetailPage {
-          
+        if let detailPageAsset = detailPageAsset {
+
         }
     }
   }
