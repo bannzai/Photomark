d85d7e42d67213de48d2a3f56dc389937cf90dd9
bannzai <kingkong999yhirose@gmail.com>
2023-08-31 14:19:32 +0900

    Remove unnecessary code

--- KeyboardExtension/KeyboardViewController.swift
+++ KeyboardExtension/KeyboardViewController.swift
@@ -81,34 +81,6 @@ struct KeyboardView: View {
   }
 }
 
-struct NextKeyboardButton: View {
-  let systemName: String
-  let action: Selector
-
-  var body: some View {
-    Image(systemName: systemName)
-      .overlay {
-        NextKeyboardButtonOverlay(action: action)
-      }
-  }
-}
-
-struct NextKeyboardButtonOverlay: UIViewRepresentable {
-  let action: Selector
-
-  func makeUIView(context: Context) -> UIButton {
-    // UIButtonを生成し、セレクターをactionに設定
-    let button = UIButton()
-    button.addTarget(nil,
-                     action: action,
-                     for: .allTouchEvents)
-    return button
-  }
-
-  func updateUIView(_ button: UIButton, context: Context) {}
-}
-
-
 struct PhotoAssetListGrid: View {
   @Environment(\.managedObjectContext) private var viewContext
 
