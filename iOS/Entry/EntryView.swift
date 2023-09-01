97de4bc5647c6a3f8b589e8deac882cf74396997
bannzai <kingkong999yhirose@gmail.com>
2023-09-02 07:32:02 +0900

    Move to Environment dir

--- iOS/Entry/EntryView.swift
+++ iOS/Entry/EntryView.swift
@@ -24,25 +24,3 @@ struct EntryView: View {
     }
   }
 }
-
-struct ScreenSizeEnvironmentKey: EnvironmentKey {
-  static var defaultValue: CGSize = .zero
-}
-
-extension EnvironmentValues {
-  var screenSize: CGSize {
-    get {
-      self[ScreenSizeEnvironmentKey.self]
-    }
-    set {
-      self[ScreenSizeEnvironmentKey.self] = newValue
-    }
-  }
-}
-
-struct ScreenSizePreferenceKey: PreferenceKey {
-  static var defaultValue: CGSize = .zero
-  static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
-    value = nextValue()
-  }
-}
