4d48b3853cda86ea3f580041cceb66d80da9ea66
bannzai <kingkong999yhirose@gmail.com>
2023-09-02 07:18:35 +0900

    Add screen size preference

--- iOS/Entry/EntryView.swift
+++ iOS/Entry/EntryView.swift
@@ -3,11 +3,44 @@ import SwiftUI
 struct EntryView: View {
   let persistenceController = PersistenceController.shared
 
+  @State var screenSize = CGSize.zero
+
   var body: some View {
-      NavigationView {
-        PhotoAssetListPage()
-          .environment(\.managedObjectContext, persistenceController.container.viewContext)
-      }
+    NavigationView {
+      PhotoAssetListPage()
+        .background(alignment: .center) {
+          GeometryReader {
+            Color.clear.preference(key: ScreenSizePreferenceKey.self, value: $0.size)
+          }
+        }
+        .onPreferenceChange(ScreenSizePreferenceKey.self, perform: { value in
+          self.screenSize = value
+        })
+        .environment(\.screenSize, screenSize)
+        .environment(\.managedObjectContext, persistenceController.container.viewContext)
+    }
   }
 }
 
+
+struct ScreenSizeEnvironmentKey: EnvironmentKey {
+  static var defaultValue: CGSize = .zero
+}
+
+extension EnvironmentValues {
+  var screenSize: CGSize {
+    get {
+      self[ScreenSizeEnvironmentKey.self]
+    }
+    set {
+      self[ScreenSizeEnvironmentKey.self] = newValue
+    }
+  }
+}
+
+struct ScreenSizePreferenceKey: PreferenceKey {
+  static var defaultValue: CGSize = .zero
+  static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+    value = nextValue()
+  }
+}
