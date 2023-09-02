26d45e955b3045fbc2cd7cf8c41596bf05179be1
bannzai <kingkong999yhirose@gmail.com>
2023-09-02 21:03:53 +0900

    Fix property

--- KeyboardExtension/KeyboardViewController.swift
+++ KeyboardExtension/KeyboardViewController.swift
@@ -121,9 +121,36 @@ struct KeyboardView: View {
             }
           }
       }
-      PhotoAssetListGrid(assets: assets, photos: photos.toArray(), tags: tags.toArray())
+      PhotoAssetListGrid(assets: filteredAssets, photos: photos.toArray(), tags: tags.toArray())
     }
   }
+
+  var filteredAssets: [Asset] {
+    if selectedTags.isEmpty {
+      return assets
+    } else {
+      let filteredPhotos: [(photo: Photo, photoTagIDs: [String])] = photos.toArray().compactMap { photo in
+        if let tagIDs = photo.tagIDs {
+          return (photo: photo, photoTagIDs: tagIDs)
+        } else {
+          return nil
+        }
+      }.filter { tuple in
+        if !selectedTags.isEmpty {
+          return tuple.photoTagIDs.contains { photoTagID in
+            selectedTags.allSatisfy { $0.id?.uuidString == photoTagID }
+          }
+        } else {
+          return true
+        }
+      }
+
+      return assets.filter { asset in
+        filteredPhotos.contains { tuple in asset.cloudIdentifier == tuple.photo.phAssetCloudIdentifier }
+      }
+    }
+  }
+
 
 }
 
