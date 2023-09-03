84f5d06b6f16f1a02fae1f02011df74ecc962deb
bannzai <kingkong999yhirose@gmail.com>
2023-09-03 11:50:16 +0900

    Fix VGrid

--- Shared/Components/VGrid.swift
+++ Shared/Components/VGrid.swift
@@ -7,7 +7,7 @@ struct VGrid<E, Content: View>: View {
   @ViewBuilder let content: (E) -> Content
 
   var body: some View {
-    Grid(horizontalSpacing: spacing) {
+    Grid(alignment: .leading, horizontalSpacing: spacing, verticalSpacing: spacing) {
       let chunkedList = elements.chunked(by: gridCount)
       ForEach(0..<chunkedList.count, id: \.self) { i in
         let chunked = chunkedList[i]
