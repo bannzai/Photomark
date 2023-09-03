9af8a755623ee528cdc0ab4332dd0bd314e913df
bannzai <kingkong999yhirose@gmail.com>
2023-09-03 11:31:16 +0900

    remove unnecessary spacer

--- Shared/Components/VGrid.swift
+++ Shared/Components/VGrid.swift
@@ -7,7 +7,7 @@ struct VGrid<E, Content: View>: View {
   @ViewBuilder let content: (E) -> Content
 
   var body: some View {
-    Grid {
+    Grid(horizontalSpacing: spacing) {
       let chunkedList = elements.chunked(by: gridCount)
       ForEach(0..<chunkedList.count, id: \.self) { i in
         let chunked = chunkedList[i]
@@ -15,11 +15,6 @@ struct VGrid<E, Content: View>: View {
         GridRow {
           ForEach(0..<chunked.count, id: \.self) { j in
             content(chunked[j])
-
-            if chunked.count - 1 != j {
-              Spacer()
-                .frame(width: spacing)
-            }
           }
         }
       }
